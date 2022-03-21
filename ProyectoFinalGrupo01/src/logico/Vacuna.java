71fedd15d4b7271bf063268985e8f793c048fb22
eleaz <eleaz@MSI>
2022-03-20 21:09:48 -0400

    Se cargaron datos a vacuna.

--- ProyectoFinalGrupo01/src/logico/Vacuna.java
+++ ProyectoFinalGrupo01/src/logico/Vacuna.java
@@ -1,5 +1,42 @@
 package logico;
 
+import java.util.Date;
+
 public class Vacuna {
+	private Date fecha;
+	private String lugarString;
+	private String vacunaString;
+	
+	public Vacuna(Date fecha, String lugarString, String vacunaString) {
+		super();
+		this.fecha = fecha;
+		this.lugarString = lugarString;
+		this.vacunaString = vacunaString;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getLugarString() {
+		return lugarString;
+	}
+
+	public void setLugarString(String lugarString) {
+		this.lugarString = lugarString;
+	}
+
+	public String getVacunaString() {
+		return vacunaString;
+	}
+
+	public void setVacunaString(String vacunaString) {
+		this.vacunaString = vacunaString;
+	}
+	
 
 }

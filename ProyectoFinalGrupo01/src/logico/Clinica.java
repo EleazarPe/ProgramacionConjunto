5705af3bba817694c1627487a81074cc1cc75ea4
eleaz <eleaz@MSI>
2022-03-22 21:38:16 -0400

    llene la controladora agregandole el Instance y insertarUsuario.

--- ProyectoFinalGrupo01/src/logico/Clinica.java
+++ ProyectoFinalGrupo01/src/logico/Clinica.java
@@ -1,5 +1,64 @@
 package logico;
 
-public class Clinica {
+import java.util.ArrayList;
 
+public class Clinica {
+	private ArrayList<Usuario> usuarios;
+	private ArrayList<Vacuna> vacunas;
+	private ArrayList<Consulta> consultas;
+	private ArrayList<Vigilancia> vigilancias;
+	private ArrayList<Historial> historiales;
+	private static Clinica miclinica= null;
+	
+	public Clinica() {
+		super();
+		this.usuarios = new ArrayList<>();
+		this.vacunas = new ArrayList<>();
+		this.consultas = new ArrayList<>();
+		this.vigilancias = new ArrayList<>();
+		this.historiales = new ArrayList<>();
+	}
+	
+	public static Clinica getInstance() {
+		if(miclinica==null) {
+			miclinica = new Clinica();
+		}
+		return miclinica;
+	}
+	
+	public ArrayList<Usuario> getUsuarios() {
+		return usuarios;
+	}
+	public void setUsuarios(ArrayList<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+	public ArrayList<Vacuna> getVacunas() {
+		return vacunas;
+	}
+	public void setVacunas(ArrayList<Vacuna> vacunas) {
+		this.vacunas = vacunas;
+	}
+	public ArrayList<Consulta> getConsultas() {
+		return consultas;
+	}
+	public void setConsultas(ArrayList<Consulta> consultas) {
+		this.consultas = consultas;
+	}
+	public ArrayList<Vigilancia> getVigilancias() {
+		return vigilancias;
+	}
+	public void setVigilancias(ArrayList<Vigilancia> vigilancias) {
+		this.vigilancias = vigilancias;
+	}
+	public ArrayList<Historial> getHistoriales() {
+		return historiales;
+	}
+	public void setHistoriales(ArrayList<Historial> historiales) {
+		this.historiales = historiales;
+	}
+	
+	void insertarUsuario(Usuario us){
+		usuarios.add(us);
+	}
+	
 }

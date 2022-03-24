1813f32695ff9e624d049c1eb9ca814f733cf5e9
Ivan <Ivan@DESKTOP-ACHU2Q0>
2022-03-23 22:49:37 -0400

    Update a la parte visual de Crear citas, y lo logico, hare unos ajustes luego

--- ProyectoFinalGrupo01/src/logico/Paciente.java
+++ ProyectoFinalGrupo01/src/logico/Paciente.java
@@ -6,12 +6,15 @@ public class Paciente extends Usuario {
 
 	private ArrayList<Consulta> misConsultas;
 	private String tipoSangre;
+	private String CorreoElectronico;
 	
 	public Paciente(String nombre, String apellido, String cedula, Date fechaNaciento, String ocupacion,
-			String telefono, String direccion,String codigo,ArrayList<Consulta>misConsultas, String tipoSangre) {
+			String telefono, String direccion,String codigo,ArrayList<Consulta>misConsultas, String tipoSangre,
+			String correoElectronico) {
 		super(nombre, apellido, cedula, fechaNaciento, ocupacion, telefono, direccion,codigo);
 		this.misConsultas = new ArrayList<>();
 		this.tipoSangre = tipoSangre;
+		this.CorreoElectronico = correoElectronico;
 	}
 
 	public ArrayList<Consulta> getMisConsultas() {
@@ -28,6 +31,14 @@ public class Paciente extends Usuario {
 
 	public void setTipoSangre(String tipoSangre) {
 		this.tipoSangre = tipoSangre;
+	}
+
+	public String getCorreoElectronico() {
+		return CorreoElectronico;
+	}
+
+	public void setCorreoElectronico(String correoElectronico) {
+		CorreoElectronico = correoElectronico;
 	}
 
 }
--- ProyectoFinalGrupo01/src/visual/CrearCita.java
+++ ProyectoFinalGrupo01/src/visual/CrearCita.java
@@ -17,16 +17,22 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.SpinnerDateModel;
+import java.util.Date;
+import java.util.Calendar;
+import java.awt.FlowLayout;
 
 public class CrearCita extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
+	private JTextField txtNombre;
+	private JTextField txtApellido;
+	private JTextField txtCelular;
+	private JTextField txtTelefonoOpc;
+	private JTextField TxtCorreo;
+	private JTextField txtNid;
+	private JRadioButton rdbtnNo;
+	private JRadioButton rdbtnNewRadioButton;
 
 	/**
 	 * Launch the application.
@@ -48,8 +54,9 @@ public class CrearCita extends JFrame {
 	 * Create the frame.
 	 */
 	public CrearCita() {
+		setTitle("Registro De Citas");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 752, 463);
+		setBounds(100, 100, 752, 391);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -57,17 +64,17 @@ public class CrearCita extends JFrame {
 		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(12, 13, 710, 393);
+		panel.setBounds(12, 13, 710, 304);
 		panel.setBorder(new TitledBorder(null, "Datos del Paciente:", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Nombres:");
-		lblNewLabel.setBounds(22, 163, 70, 25);
+		lblNewLabel.setBounds(33, 139, 70, 25);
 		panel.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Apellidos:");
-		lblNewLabel_1.setBounds(367, 163, 70, 25);
+		lblNewLabel_1.setBounds(378, 139, 70, 25);
 		panel.add(lblNewLabel_1);
 		
 		JButton button = new JButton("Buscar    ");
@@ -77,30 +84,30 @@ public class CrearCita extends JFrame {
 		panel.add(button);
 		
 		JLabel lblNewLabel_2 = new JLabel("Menor de Edad:");
-		lblNewLabel_2.setBounds(12, 201, 104, 25);
+		lblNewLabel_2.setBounds(21, 177, 104, 25);
 		panel.add(lblNewLabel_2);
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("SI");
-		rdbtnNewRadioButton.setBounds(124, 201, 48, 25);
+		rdbtnNewRadioButton = new JRadioButton("SI");
+		rdbtnNewRadioButton.setBounds(124, 177, 48, 25);
 		panel.add(rdbtnNewRadioButton);
 		
-		JRadioButton rdbtnNo = new JRadioButton("NO");
+		rdbtnNo = new JRadioButton("NO");
 		rdbtnNo.setSelected(true);
-		rdbtnNo.setBounds(176, 201, 48, 25);
+		rdbtnNo.setBounds(176, 177, 48, 25);
 		panel.add(rdbtnNo);
 		
-		textField = new JTextField();
-		textField.setBounds(94, 163, 258, 25);
-		panel.add(textField);
-		textField.setColumns(10);
+		txtNombre = new JTextField();
+		txtNombre.setBounds(94, 139, 258, 25);
+		panel.add(txtNombre);
+		txtNombre.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(440, 163, 258, 25);
-		panel.add(textField_1);
+		txtApellido = new JTextField();
+		txtApellido.setColumns(10);
+		txtApellido.setBounds(440, 139, 258, 25);
+		panel.add(txtApellido);
 		
 		JLabel lblDocIdentidad = new JLabel("Doc. Identidad:");
-		lblDocIdentidad.setBounds(23, 54, 93, 25);
+		lblDocIdentidad.setBounds(33, 54, 93, 25);
 		panel.add(lblDocIdentidad);
 		
 		JComboBox comboBox = new JComboBox();
@@ -112,59 +119,41 @@ public class CrearCita extends JFrame {
 		lblNewLabel_3.setBounds(276, 54, 34, 25);
 		panel.add(lblNewLabel_3);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(307, 55, 130, 25);
-		panel.add(textField_2);
-		textField_2.setColumns(10);
+		txtNid = new JTextField();
+		txtNid.setBounds(307, 55, 130, 25);
+		panel.add(txtNid);
+		txtNid.setColumns(10);
 		
 		JLabel lblNewLabel_4 = new JLabel("Fecha de Nacimiento:");
-		lblNewLabel_4.setBounds(12, 125, 130, 25);
+		lblNewLabel_4.setBounds(33, 101, 130, 25);
 		panel.add(lblNewLabel_4);
 		
-		JSpinner spinner = new JSpinner();
-		spinner.setModel(new SpinnerNumberModel(0, 0, 30, 1));
-		spinner.setBounds(145, 126, 58, 22);
-		panel.add(spinner);
-		
-		JSpinner spinner_1 = new JSpinner();
-		spinner_1.setModel(new SpinnerNumberModel(0, 0, 12, 1));
-		spinner_1.setBounds(221, 126, 58, 22);
-		panel.add(spinner_1);
-		
-		JLabel lblNewLabel_5 = new JLabel("Dia:");
-		lblNewLabel_5.setBounds(147, 105, 56, 25);
-		panel.add(lblNewLabel_5);
-		
-		JLabel lblMes = new JLabel("Mes:");
-		lblMes.setBounds(223, 105, 56, 25);
-		panel.add(lblMes);
-		
 		JLabel lblCelular = new JLabel("Celular:");
-		lblCelular.setBounds(22, 239, 104, 25);
+		lblCelular.setBounds(33, 216, 104, 25);
 		panel.add(lblCelular);
 		
 		JLabel lblTelefonoOpcional = new JLabel("Telf. Opcional:");
-		lblTelefonoOpcional.setBounds(240, 240, 104, 25);
+		lblTelefonoOpcional.setBounds(240, 216, 104, 25);
 		panel.add(lblTelefonoOpcional);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(89, 241, 135, 22);
-		panel.add(textField_3);
-		textField_3.setColumns(10);
+		txtCelular = new JTextField();
+		txtCelular.setBounds(89, 217, 135, 22);
+		panel.add(txtCelular);
+		txtCelular.setColumns(10);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(330, 241, 132, 22);
-		panel.add(textField_4);
-		textField_4.setColumns(10);
+		txtTelefonoOpc = new JTextField();
+		txtTelefonoOpc.setBounds(330, 217, 132, 22);
+		panel.add(txtTelefonoOpc);
+		txtTelefonoOpc.setColumns(10);
 		
 		JLabel lblC = new JLabel("Correo Electronico:");
-		lblC.setBounds(22, 279, 118, 25);
+		lblC.setBounds(33, 254, 104, 25);
 		panel.add(lblC);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(134, 279, 155, 22);
-		panel.add(textField_5);
+		TxtCorreo = new JTextField();
+		TxtCorreo.setColumns(10);
+		TxtCorreo.setBounds(155, 255, 155, 22);
+		panel.add(TxtCorreo);
 		
 		JLabel lblNewLabel_6 = new JLabel("");
 		lblNewLabel_6.setIcon(new ImageIcon(CrearCita.class.getResource("/img/5097348 (2).png")));
@@ -173,22 +162,49 @@ public class CrearCita extends JFrame {
 		
 		JLabel label = new JLabel("");
 		label.setIcon(new ImageIcon(CrearCita.class.getResource("/img/5097348 (2).png")));
-		label.setBounds(355, 163, 16, 25);
+		label.setBounds(355, 139, 16, 25);
 		panel.add(label);
 		
 		JLabel label_1 = new JLabel("");
 		label_1.setIcon(new ImageIcon(CrearCita.class.getResource("/img/5097348 (2).png")));
-		label_1.setBounds(12, 163, 16, 25);
+		label_1.setBounds(12, 139, 16, 25);
 		panel.add(label_1);
 		
 		JLabel label_2 = new JLabel("");
 		label_2.setIcon(new ImageIcon(CrearCita.class.getResource("/img/5097348 (2).png")));
-		label_2.setBounds(12, 239, 16, 25);
+		label_2.setBounds(12, 215, 16, 25);
 		panel.add(label_2);
 		
 		JLabel label_3 = new JLabel("");
 		label_3.setIcon(new ImageIcon(CrearCita.class.getResource("/img/5097348 (2).png")));
-		label_3.setBounds(12, 279, 16, 25);
+		label_3.setBounds(12, 255, 16, 25);
 		panel.add(label_3);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setModel(new SpinnerDateModel(new Date(1648008000000L), new Date(1648008000000L), null, Calendar.DAY_OF_YEAR));
+		spinner.setEditor(new JSpinner.DateEditor(spinner,"dd/MM/yyyy"));
+		spinner.setBounds(167, 103, 137, 25);
+		panel.add(spinner);
+		
+		JLabel label_4 = new JLabel("");
+		label_4.setIcon(new ImageIcon(CrearCita.class.getResource("/img/5097348 (2).png")));
+		label_4.setBounds(12, 101, 16, 25);
+		panel.add(label_4);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.setBounds(0, 318, 736, 38);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JButton button_1 = new JButton("Cancelar");
+		button_1.setBounds(538, 7, 85, 23);
+		button_1.setActionCommand("Cancel");
+		panel_1.add(button_1);
+		
+		JButton button_2 = new JButton("Registrar");
+		button_2.setBounds(633, 7, 96, 23);
+		button_2.setActionCommand("OK");
+		panel_1.add(button_2);
 	}
 }

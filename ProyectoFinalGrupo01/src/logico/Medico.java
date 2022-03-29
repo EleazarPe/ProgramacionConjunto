6adf72883a858e222b8e46a6eb20761441730c05
eleaz <eleaz@MSI>
2022-03-28 23:50:31 -0400

    serealisable

--- ProyectoFinalGrupo01/src/logico/Medico.java
+++ ProyectoFinalGrupo01/src/logico/Medico.java
@@ -1,9 +1,14 @@
 package logico;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Medico extends Usuario {
+public class Medico extends Usuario implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String loginString;
 	private String passowrdString;
 	private String consultorioString;

cc437952d32b9a76406b7f8584a3fe9376fe9a77
eleaz <eleaz@MSI>
2022-03-30 08:18:39 -0400

    setter y getter de vacuna.

--- ProyectoFinalGrupo01/src/logico/Vacuna.java
+++ ProyectoFinalGrupo01/src/logico/Vacuna.java
@@ -1,11 +1,62 @@
 package logico;
 
+import java.util.ArrayList;
+
 public class Vacuna {
 	private String codigo;
 	private String nombreString;
 	private String laboratorioString;
 	private String administracionString;
-	private Enfermedad vigilancia;
+	private ArrayList<Enfermedad> enfermedades;
 	
+	public Vacuna(String codigo, String nombreString, String laboratorioString, String administracionString,
+			ArrayList<Enfermedad> enfermedades) {
+		super();
+		this.codigo = codigo;
+		this.nombreString = nombreString;
+		this.laboratorioString = laboratorioString;
+		this.administracionString = administracionString;
+		this.enfermedades = new ArrayList<Enfermedad>();
+	}
+
+	public String getNombreString() {
+		return nombreString;
+	}
+
+	public void setNombreString(String nombreString) {
+		this.nombreString = nombreString;
+	}
+
+	public String getLaboratorioString() {
+		return laboratorioString;
+	}
+
+	public void setLaboratorioString(String laboratorioString) {
+		this.laboratorioString = laboratorioString;
+	}
+
+	public String getAdministracionString() {
+		return administracionString;
+	}
+
+	public void setAdministracionString(String administracionString) {
+		this.administracionString = administracionString;
+	}
+
+	public ArrayList<Enfermedad> getEnfermedades() {
+		return enfermedades;
+	}
+
+	public void setEnfermedades(ArrayList<Enfermedad> enfermedades) {
+		this.enfermedades = enfermedades;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void insertarEnfermedad(Enfermedad ef) {
+		enfermedades.add(ef);
+	}
 	
 }

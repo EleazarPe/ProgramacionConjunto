755e99781fdf8da7d8f1a798fd0ceabdff353f7c
agc_g <agc_g@DESKTOP-1UMI1UA>
2022-04-04 23:49:29 -0400

    Login casi listo! 4/4/2022

--- ProyectoFinalGrupo01/src/visual/Login.java
+++ ProyectoFinalGrupo01/src/visual/Login.java
@@ -1,6 +1,8 @@
 package visual;
 
 import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -9,45 +11,61 @@ import javax.swing.JOptionPane;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Image;
+import java.awt.TextField;
+
+
 import javax.swing.border.MatteBorder;
+
+
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
 import java.awt.Toolkit;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import javax.swing.SwingConstants;
 
 public class Login extends JDialog {
-
+	
+	private Image img_logo = new ImageIcon(Login.class.getResource("/img/hospital.png")).getImage().getScaledInstance(90,90,Image.SCALE_SMOOTH);
+	private Image img_user = new ImageIcon(Login.class.getResource("/img/profile.png")).getImage().getScaledInstance(40,40,Image.SCALE_SMOOTH);
+	private Image img_pass = new ImageIcon(Login.class.getResource("/img/padlock.png")).getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+	private Image img_login = new ImageIcon(Login.class.getResource("/img/cerrar-sesion.png")).getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
 	
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
-	private JPasswordField pwdContrasea;
 	private JTextField txtUsuario;
-
+	
 	
 	public static void main(String[] args) {
 		try {
 			Login dialog = new Login();
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
 
-
-	public Login() {
+	public Login() {	
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Login.class.getResource("/img/cruz-roja.png")));
 		setBounds(100, 100, 600, 450);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setLocationRelativeTo(null);
+		setModal(true);
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(new BorderLayout(0, 0));
 		setUndecorated(true);
 		{
+			
 			JPanel panel = new JPanel();
 			panel.setBackground(new Color(0, 0, 139));
 			contentPanel.add(panel, BorderLayout.CENTER);
@@ -83,30 +101,34 @@ public class Login extends JDialog {
 			panel_1.add(LblSalir);
 			
 			JPanel panel_2 = new JPanel();
-			panel_2.setBounds(175, 116, 200, 40);
+			panel_2.setBounds(185, 179, 200, 40);
 			panel_1.add(panel_2);
 			panel_2.setLayout(null);
 			
 			txtUsuario = new JTextField();
-			txtUsuario.setBounds(0, 0, 150, 40);
-			panel_2.add(txtUsuario);
 			txtUsuario.addFocusListener(new FocusAdapter() {
 				@Override
-				public void focusGained(FocusEvent arg0) {
+				public void focusGained(FocusEvent e) {
 					if(txtUsuario.getText().equals("Usuario")) {
 						txtUsuario.setText("");
 					}
 					else {
 						txtUsuario.selectAll();
 					}
+					
 				}
 				@Override
 				public void focusLost(FocusEvent e) {
-					if(txtUsuario.getText().equals(""))
+					if(txtUsuario.getText().equals("")) {
 						txtUsuario.setText("Usuario");
+					}
 				}
 			});
+			txtUsuario.setBorder(null);
 			txtUsuario.setText("Usuario");
+			txtUsuario.setBounds(0, 0, 150, 40);
+			panel_2.add(txtUsuario);
+		
 			
 			txtUsuario.setToolTipText("");
 			txtUsuario.addMouseListener(new MouseAdapter() {
@@ -115,49 +137,74 @@ public class Login extends JDialog {
 			});
 			txtUsuario.setColumns(10);
 			
+			JLabel lblUserLogo = new JLabel("");
+			lblUserLogo.setHorizontalAlignment(SwingConstants.CENTER);
+			lblUserLogo.setBounds(131, 0, 88, 40);
+			panel_2.add(lblUserLogo);
+			lblUserLogo.setIcon(new ImageIcon(img_user));
+			
+			
 			JPanel panel_3 = new JPanel();
-			panel_3.setLayout(null);
-			panel_3.setBounds(175, 185, 200, 40);
+			panel_3.setBounds(185, 248, 200, 40);
 			panel_1.add(panel_3);
 			
-			pwdContrasea = new JPasswordField();
-			pwdContrasea.setBounds(0, 0, 150, 40);
-			panel_3.add(pwdContrasea);
-			pwdContrasea.addFocusListener(new FocusAdapter() {
-				@SuppressWarnings("deprecation")
+			JPasswordField pwdContraseña = new JPasswordField();
+			pwdContraseña.setBounds(0, 0, 150, 40);
+			pwdContraseña.addFocusListener(new FocusAdapter() {
 				@Override
 				public void focusGained(FocusEvent e) {
-					if(pwdContrasea.getText().equals("contraseña")) {
-						pwdContrasea.setEchoChar('●');
-						pwdContrasea.setText("");	
+					if(pwdContraseña.getText().equals("Contraseña")) {
+						pwdContraseña.setEchoChar('●');
+						pwdContraseña.setText("");
 					}
 					else {
-						pwdContrasea.selectAll();
+						pwdContraseña.selectAll();
 					}
+					
 				}
-				@SuppressWarnings("deprecation")
 				@Override
 				public void focusLost(FocusEvent e) {
-					if(pwdContrasea.getText().equals(""))
-						pwdContrasea.setText("Contraseña");
-						pwdContrasea.setEchoChar((char)0);
+					if(pwdContraseña.getText().equals("")) {
+						pwdContraseña.setText("Contraseña");
+						pwdContraseña.setEchoChar((char)0);
+					}
 				}
 			});
-			pwdContrasea.setText("Contrase\u00F1a");
-			pwdContrasea.setToolTipText("");
-			pwdContrasea.setEchoChar((char)0);
+			panel_3.setLayout(null);
+			pwdContraseña.setBorder(null);
+			pwdContraseña.setEchoChar((char)0);
+			panel_3.add(pwdContraseña);
+			pwdContraseña.setText("Contraseña");
+			pwdContraseña.setToolTipText("");
+			
+			JLabel lblPassLogo = new JLabel("");
+			lblPassLogo.setBounds(151, -12, 49, 64);
+			lblPassLogo.setHorizontalAlignment(SwingConstants.CENTER);
+			panel_3.add(lblPassLogo);
+			lblPassLogo.setIcon(new ImageIcon(img_pass));
 			
 			JPanel panel_4 = new JPanel();
-			panel_4.setBounds(196, 252, 159, 31);
+			panel_4.setBounds(212, 324, 145, 47);
 			panel_1.add(panel_4);
+			panel_4.setLayout(null);
 			
 			JLabel lblNewLabel = new JLabel("Iniciar Sesion");
-			lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
+			lblNewLabel.setBounds(41, 13, 95, 20);
+			lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
 			panel_4.add(lblNewLabel);
-			pwdContrasea.addMouseListener(new MouseAdapter() {
-				
-
-			});
+			
+			JLabel lblLoginLogo = new JLabel("");
+			lblLoginLogo.setBounds(0, 8, 30, 30);
+			panel_4.add(lblLoginLogo);
+			lblLoginLogo.setIcon(new ImageIcon(img_login));
+			
+			JLabel lblLogo = new JLabel("");
+			lblLogo.setHorizontalAlignment(SwingConstants.CENTER);
+			lblLogo.setBounds(196, 61, 177, 108);
+			panel_1.add(lblLogo);
+			lblLogo.setIcon(new ImageIcon(img_logo));
+			
 		}
+		
 	}
 }

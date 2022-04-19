48d7ccffac4b306cf0f456abb104ec64c6e7567b
eleaz <eleaz@MSI>
2022-04-19 01:15:47 -0400

    reporte listas

--- ProyectoFinalGrupo01/src/visual/ReporteList.java
+++ ProyectoFinalGrupo01/src/visual/ReporteList.java
@@ -17,17 +17,27 @@ import javax.swing.JTable;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
 
 public class ReporteList extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
 	private JTable table;
-	private JTable table_1;
+	private JTable table1;
 	private JLabel vacunalbl;
 	private JLabel enfermoslbl;
 	private JPanel pnlVacunas;
 	private JPanel pnlGrafico;
 	private JPanel pnlEnfermedades;
+	private DefaultTableModel model1;
+	private Object row1[];
+	private DefaultTableModel model2;
+	private Object row2[];
+	private boolean control = false;
+	private JToggleButton tglbtnEnfermedades;
+	private JToggleButton tglbtnVacuna;
+	private JToggleButton tglbtnGrafico;
 
 	/**
 	 * Launch the application.
@@ -61,9 +71,13 @@ public class ReporteList extends JDialog {
 		JScrollPane scrollPane = new JScrollPane();
 		pnlEnfermedades.add(scrollPane, BorderLayout.CENTER);
 		{
+			String headers[] = {"Matricula","Eslora","Fabricacion"};
+			model2 = new DefaultTableModel();
+			model2.setColumnIdentifiers(headers);
 			table = new JTable();
-			scrollPane.setViewportView(table);
 		}
+		table.setModel(model2);
+		scrollPane.setViewportView(table);
 		{
 			pnlVacunas = new JPanel();
 			contentPanel.add(pnlVacunas, BorderLayout.CENTER);
@@ -72,25 +86,35 @@ public class ReporteList extends JDialog {
 				JScrollPane scrollPane_1 = new JScrollPane();
 				pnlVacunas.add(scrollPane_1, BorderLayout.CENTER);
 				{
-					table_1 = new JTable();
-					scrollPane_1.setViewportView(table_1);
+					String headers[] = {"Matricula","Eslora","Fabricacion"};
+					model1 = new DefaultTableModel();
+					model1.setColumnIdentifiers(headers);
+					table1 = new JTable();
+					table1.setModel(model1);
+					scrollPane_1.setViewportView(table1);
 				}
 			}
 		}
 		{
 			pnlGrafico = new JPanel();
 			contentPanel.add(pnlGrafico, BorderLayout.CENTER);
+			pnlGrafico.setLayout(null);
 			{
-				vacunalbl = new JLabel("");
+				vacunalbl = new JLabel("10");
+				vacunalbl.setVisible(false);
+				//vacunalbl.setBounds(158, 102, 12, 14);
 				pnlGrafico.add(vacunalbl);
 			}
 			{
-				enfermoslbl = new JLabel("");
+				enfermoslbl = new JLabel("10");
+				enfermoslbl.setVisible(false);
+				//enfermoslbl.setBounds(461, 102, 12, 14);
 				pnlGrafico.add(enfermoslbl);
 			}
 		}
 		{
 			JPanel buttonPane = new JPanel();
+			buttonPane.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
@@ -109,61 +133,73 @@ public class ReporteList extends JDialog {
 			JMenuBar menuBar = new JMenuBar();
 			setJMenuBar(menuBar);
 			{
-				JToggleButton tglbtnNewToggleButton = new JToggleButton("Enfermedades");
-				tglbtnNewToggleButton.addActionListener(new ActionListener() {
+				tglbtnEnfermedades = new JToggleButton("Enfermedades");
+				tglbtnEnfermedades.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						//----------------------------------->>>>>>>>
+						tglbtnGrafico.setSelected(false);
+						tglbtnVacuna.setSelected(false);
 						pnlVacunas.setVisible(false);
 						pnlGrafico.setVisible(false);
 						pnlEnfermedades.setVisible(true);
+						control = false;
 					}
 				});
-				menuBar.add(tglbtnNewToggleButton);
+				menuBar.add(tglbtnEnfermedades);
 			}
 			{
-				JToggleButton tglbtnNewToggleButton_1 = new JToggleButton("Vacunas");
-				tglbtnNewToggleButton_1.addActionListener(new ActionListener() {
+				tglbtnVacuna = new JToggleButton("Vacunas");
+				tglbtnVacuna.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						//----------------------------------->>>>>>>>
+						tglbtnEnfermedades.setSelected(false);
+						tglbtnGrafico.setSelected(false);
 						pnlVacunas.setVisible(true);
 						pnlGrafico.setVisible(false);
 						pnlEnfermedades.setVisible(false);
+						control = false;
 					}
 				});
-				menuBar.add(tglbtnNewToggleButton_1);
+				menuBar.add(tglbtnVacuna);
 			}
 			{
-				JToggleButton tglbtnNewToggleButton_2 = new JToggleButton("Grafico de Barra");
-				tglbtnNewToggleButton_2.addActionListener(new ActionListener() {
+				tglbtnGrafico = new JToggleButton("Grafico de Barra");
+				tglbtnGrafico.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						
+						tglbtnEnfermedades.setSelected(false);
+						tglbtnVacuna.setSelected(false);
 						pnlVacunas.setVisible(false);
 						pnlGrafico.setVisible(true);
 						pnlEnfermedades.setVisible(false);
+						control = true;
+						vacunalbl.setVisible(true);
+						enfermoslbl.setVisible(true);
 						repaint();
 					}
 				});
-				menuBar.add(tglbtnNewToggleButton_2);
+				menuBar.add(tglbtnGrafico);
 			}
 		}
 	}
 	
     public void paint(Graphics g) {
     	super.paint(g);
-    	int alto1 = 13*10;//buscaCantidadDeVacunas()
-    	int alto2 = 4*10;
-    	int tama = 200;
-    	
-    	g.setColor(new Color(255,0,0));
-    	g.fill3DRect(100,tama-alto1, 50, alto1, true);
-		vacunalbl.setBounds(100, (tama-alto1)-60 , 50, 20);
-    	g.drawString("Vacuna", 100, 225);
-    	
-    	g.setColor(new Color(32,178,170));
-    	g.fill3DRect(180,tama-alto2, 50, alto2, true);
-		enfermoslbl.setBounds(180, (tama-alto2)-60 , 50, 20);
-    	g.drawString("Enfermos", 180, 225);
-    	
+    	int alto1 = 4*20;//buscaCantidadDeVacunas()
+    	int alto2 = 10*20;
+    	int tama = 400;
+    	if(control !=false) {
+	    	g.setColor(new Color(255,0,0));
+	    	g.fill3DRect(250,tama-alto1, 50, alto1, true);
+	    	//vacunalbl.setText("100");
+			vacunalbl.setBounds(250, (tama-alto1)-90 , 50, 20);
+	    	g.drawString("Vacuna", 250, 425);
+	    	
+	    	g.setColor(new Color(32,178,170));
+	    	g.fill3DRect(325,tama-alto2, 50, alto2, true);
+			enfermoslbl.setBounds(325, (tama-alto2)-90 , 50, 20);
+	    	g.drawString("Enfermdad", 325, 425);
+    	}
     	//(32,178,170)
     }
+    
 }
